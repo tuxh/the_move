2be7c00c944b2d697e334433e9039cf2e042dca3
tuxh <tuxh@qq.com>
2020-01-06 11:59:30 +0800

    commit by idea

---
+++ demo001/src/com/Code.java
@@ -0,0 +1,22 @@
+package com;
+
+public class Code {
+   private String codeId;
+   private String name;
+
+    public String getCodeId() {
+        return codeId;
+    }
+
+    public void setCodeId(String codeId) {
+        this.codeId = codeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
